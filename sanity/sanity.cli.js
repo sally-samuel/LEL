506cfd944c884b6d873de6fd2f258c98e8ff7382
Salome Egwumba <salome.egwumba@leveneenergy.com>
2026-02-11 12:30:38 +0100

    update

--- sanity/sanity.cli.js
+++ sanity/sanity.cli.js
@@ -6,10 +6,9 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
+    
     autoUpdates: true,
+    appId: 'o95fs47gdu1balybhtj66qfq'
+  
   }
 })
